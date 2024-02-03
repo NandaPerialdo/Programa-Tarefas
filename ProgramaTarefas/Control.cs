3eb702cf9949eef7203c5811f654ad176a6d9ede
FERNANDA PERIALDO ALVES <fernanda.palves3@senacsp.edu.br>
2024-02-02 22:11:57 -0300

    Terceiro Commit

--- ProgramaTarefas/Control.cs
+++ ProgramaTarefas/Control.cs
@@ -11,6 +11,8 @@ namespace ProgramaTarefas
         DAO conectar;// criando uma variavel (conectar) para representar
                      // o DAO
         private int opcao;
+        public int ConsultarOpcaoTarefas;
+
         public ControlUsuario() //criando o metodo contrutor
         {
             ConsultarOpcao = 0;
@@ -42,6 +44,29 @@ namespace ProgramaTarefas
                 {
                     case 1:
                         //fazer login
+                        do
+                        {
+                            MenuTarefas();
+                            switch (ConsultarOpcaoTarefas)
+                            {
+                                case 1:
+                                    //criar tarefa
+                                    break;
+                                case 2:
+                                    //alterar tarefa
+                                    break;
+                                case 3:
+                                    //excluir tarefa 
+                                    break;
+                                case 4:
+                                    //consultar tarefa
+                                    break;
+                                default:
+                                    //caso o usuario insira um numero diferente de 4
+                                    Console.WriteLine("Erro! Escolha uma opção entre 1 e 4");
+                                    break;
+                            }//fim do escolha caso
+                        } while (ConsultarOpcaoTarefas != 4);
                         break;
                     case 2:
                         //fazer cadastro
@@ -49,6 +74,7 @@ namespace ProgramaTarefas
                         break;
                     case 3:
                         //sair
+                        Console.WriteLine("Obrigado!");
                         break;
                     default: //caso o usuário insira uma "case" que não exista
                         Console.WriteLine("Erro! Informe uma opção válida");
@@ -73,10 +99,37 @@ namespace ProgramaTarefas
             Console.WriteLine("Insira seu nome de senha: ");
             string senha = Console.ReadLine();
             //inserir no banco de dados
-            conectar.InserirUsuario(nome,telefone,email,datanasc, login, senha);
+            conectar.InserirUsuario(nome, telefone, email, datanasc, login, senha);
         }//fim do metodo cadastrar usuario
+
+        public void MenuTarefas()
+        {
+            Console.WriteLine("Escolha uma opção:\n" +
+                              "1. Criar Tarefa.\n" +
+                              "2. Alterar Tarefa.\n" +
+                              "3. Excluir Tarefa.\n" +
+                              "4. Consultar Tarefa.\n");
+            int ConsultarOpcaoTarefas = Convert.ToInt32(Console.ReadLine());
+    }//fim do menu tarefa
+
+        public void CriarTarefa()
+        {
+            Console.WriteLine("Qual o nome da tarefa?");
+            nomeTarefa = Console.ReadLine();
+            Console.WriteLine("Qual a descrição?");
+            descricaoTarefa = Console.ReadLine();
+            Console.WriteLine("Qual a data de execução?");
+            dtTarefa = Console.WriteLine();
+            //inserir no banco de dados
+            conectar.InserirTarefa(nomeTarefa, descricaoTarefa, dtTarefa);
+        }//fim do metodo criar tarefa
+
+
+
+
 
 
 
     }// fim da classe
+
 }//fim do projeto
--- ProgramaTarefas/DAO.cs
+++ ProgramaTarefas/DAO.cs
@@ -18,15 +18,21 @@ namespace ProgramaTarefas
         public string dados;
         public string sql;
         public string resultado;
-        public string nome;//declarando as variaveis que serao usadas pra preencher o banco
-        public string telefone;
-        public string email;
-        public string datanasc;
-        public string login;
-        public string senha;
+        public int[] codigo;
+        public string[] nome;//declarando as variaveis que serao usadas pra preencher o banco
+        public string[] telefone;
+        public string[] email;
+        public string[] dtnasc;
+        public string[] login;
+        public string[] senha;
+        public int i;
+        public int contador;
+        public string[] nomeTarefa;
+        public string[] descricaoTarefa;
+        public string[] dtTarefa;
         public DAO()
         {
-            conexao = new MySqlConnection("server=localhost;DataBase=Pessoa;Uid=root;Password="); //estabelecendo conexao da classe MySqlConnection com a variavel conexao 
+            conexao = new MySqlConnection("server=localhost;DataBase=ProgramaTarefas;Uid=root;Password="); //estabelecendo conexao da classe MySqlConnection com a variavel conexao 
             try // tratar o caso de se a conexao nao for completa
             {
                 conexao.Open();//abrindo a conexao com o banco de dados
@@ -39,12 +45,12 @@ namespace ProgramaTarefas
             }//fim do try catch
         }//fim do metodo DAO
 
-        public void InserirUsuario (string nome, string telefone, string email, string datanasc, string login, string senha)
+        public void InserirUsuario (string nome, string telefone, string email, string dtnasc, string login, string senha)
         {
             try
             {
-                dados = "('','" + nome + "', '" + telefone + "','" + email + "','" + datanasc + "','" + login + "','" + senha + "')";
-                sql = "insert into usuario(codigo, nome, telefone, email, datanasc, login, senha) values" + dados;//essa linha representa entre "" o que será executado dentro do banco de dados
+                dados = "('','" + nome + "', '" + telefone + "','" + email + "','" + dtnasc + "','" + login + "','" + senha + "')";
+                sql = "insert into usuario(codigo, nome, telefone, email, dtnasc, login, senha) values" + dados;//essa linha representa entre "" o que será executado dentro do banco de dados
                                                                                                                   //na linha de cima, dentro da variavel "dados" serao preenchidos os dados a serem
                                                                                                                   //inseridos, antes de executar a linha de codigo no banco de dados
 
@@ -57,6 +63,65 @@ namespace ProgramaTarefas
                 Console.WriteLine("Erro! Algo deu errado :(\n\n\n" + erro);
             }//fim do catch
         }//fim do metodo inserir usuario
+
+        public void PreencherVetorUsuario()
+        {
+            string query = "select * from usuario";
+
+            //instanciar os valores
+            codigo = new int[100];
+            nome = new string[100];
+            telefone = new string[100];
+            email = new string[100];
+            dtnasc = new string[100];
+            login = new string[100];
+            senha = new string[100];
+
+            //preencher com valores genericos
+            for(i = 0; i < 100; i++)
+            {
+                codigo[i] = 0;
+                nome[i] = "";
+                telefone[i] = "";
+                email[i] = "";
+                dtnasc[i] = "";
+                login[i] = "";
+                senha[i] = "";
+            }//fim do for
+
+            //preparando o comando para o banco
+            MySqlCommand coletar = new MySqlCommand(query, conexao);
+            //leitura do banco
+            MySqlDataReader leitura = coletar.ExecuteReader();
+
+            i = 0;
+            contador = 0;
+
+            while(leitura.Read())
+            {
+                codigo[i] = Convert.ToInt32(leitura["codigo"]);
+                nome[i] = "" + leitura["nome"];
+                telefone[i] = "" + leitura["telefone"];
+                email[i] = "" + leitura["email"];
+                dtnasc[i] = "" + leitura["dtnasc"];
+                login[i] = "" + leitura["login"];
+                senha[i] = "" + leitura["senha"];
+                i++;
+                contador++;
+            }//preenchendo o vetor com os dados do banco
+
+            leitura.Close();//encerrar o acesso ao banco de dados
+        }//fim do preencher
+
+        public void InserirTarefa(string nomeTarefa, string descricaoTarefa, string dtTarefa)
+        {
+            try
+            {
+                dados = "(''
+            }
+        }
+        
+
 
     }//fim da classe
 }//fim do projeto
